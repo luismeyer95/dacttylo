b5de862fe46d8ec1ca07bcdf8182fc34d197a88a
luismeyer95 <luismeyer95@gmail.com>
2022-02-03 21:08:00 +0100

    checkpoint

--- src/line_stylizer.rs
+++ src/line_stylizer.rs
@@ -10,9 +10,36 @@ use tui::text::StyledGrapheme;
 use unicode_segmentation::UnicodeSegmentation;
 use unicode_width::UnicodeWidthStr;
 
-pub struct LineStylizer;
+const TAB_SYMBOL: &str = "\u{21e5}";
+const NL_SYMBOL: &str = "\u{23ce}";
+const SPACE: &str = " ";
 
-impl LineProcessor for LineStylizer {
+pub struct SymbolMap {
+    pub tab: StyledGrapheme<'static>,
+    pub nl: StyledGrapheme<'static>,
+}
+
+pub struct BaseLineProcessor {
+    symbols: SymbolMap,
+}
+
+impl Default for BaseLineProcessor {
+    fn default() -> Self {
+        let empty_cell = StyledGrapheme {
+            symbol: SPACE,
+            style: tui::style::Style::default(),
+        };
+
+        Self {
+            symbols: SymbolMap {
+                tab: empty_cell.clone(),
+                nl: empty_cell,
+            },
+        }
+    }
+}
+
+impl LineProcessor for BaseLineProcessor {
     fn process_line<'txt>(
         &self,
         line: &mut dyn Iterator<Item = StyledGrapheme<'txt>>,
@@ -24,10 +51,7 @@ impl LineProcessor for LineStylizer {
     }
 }
 
-impl LineStylizer {
-    const TAB_SYMBOL: &'static str = "\u{21e5}";
-    const NL_SYMBOL: &'static str = "\u{23ce}";
-
+impl BaseLineProcessor {
     fn transform_line<'txt>(
         &self,
         line: &mut dyn Iterator<Item = StyledGrapheme<'txt>>,
@@ -45,7 +69,7 @@ impl LineStylizer {
         let mut transformed_line: Vec<StyledGrapheme> = vec![];
 
         for (key_offset, gphm) in graphemes.into_iter().enumerate() {
-            let remapped_key = Self::remap_symbol(column_offset, gphm);
+            let remapped_key = self.remap_symbol(column_offset, gphm);
             let styled_key = Self::apply_sparse_styling(
                 key_offset,
                 remapped_key,
@@ -53,7 +77,6 @@ impl LineStylizer {
             );
             let column_size: usize =
                 styled_key.iter().map(|k| k.symbol.width()).sum();
-            // let column_size = styled_key.len();
 
             transformed_line.extend(styled_key);
             column_offset += column_size;
@@ -85,7 +108,9 @@ impl LineStylizer {
         sparse_styling: &HashMap<usize, tui::style::Style>,
     ) -> Vec<StyledGrapheme<'txt>> {
         if let Some(style) = sparse_styling.get(&key_offset) {
-            key_as_graphemes[0].style = *style;
+            // key_as_graphemes[0].style = *style;
+            let mut style_ref = &mut key_as_graphemes[0].style;
+            *style_ref = style_ref.patch(*style);
         }
         key_as_graphemes
     }
@@ -96,7 +121,7 @@ impl LineStylizer {
     ) -> Vec<Vec<StyledGrapheme>> {
         let mut rows: Vec<Vec<StyledGrapheme>> = Vec::with_capacity(16);
         let mut cur_row: Vec<StyledGrapheme> = Vec::with_capacity(16);
-        let mut cur_row_width = 0usize;
+        let mut cur_row_width = 0;
 
         for g in graphemes {
             let sym_width = g.symbol.width();
@@ -116,84 +141,78 @@ impl LineStylizer {
         rows
     }
 
-    // fn wrap_line(
-    //     graphemes: Vec<StyledGrapheme>,
-    //     width: u16,
-    // ) -> Vec<Vec<StyledGrapheme>> {
-    //     graphemes
-    //         .chunks((width) as usize)
-    //         .map(|x| x.to_vec())
-    //         .collect()
-    // }
-
-    // fn wrap_line(
-    //     graphemes: Vec<StyledGrapheme>,
-    //     width: u16,
-    // ) -> Vec<Vec<StyledGrapheme>> {
-    //     // use tui::widgets::Paragraph;
-    //     let mut gphm_iter = graphemes.into_iter();
-    //     let mut ln_wrapper: Box<dyn LineComposer> =
-    //         Box::new(WordWrapper::new(&mut gphm_iter, width, false));
-
-    //     let mut rows: Vec<Vec<StyledGrapheme>> = Vec::with_capacity(16);
-
-    //     while let Some((current_line, _)) = ln_wrapper.next_line() {
-    //         rows.push(current_line.into());
-    //     }
-
-    //     rows
-    // }
-
-    fn remap_symbol(
+    fn remap_symbol<'txt>(
+        &self,
         inline_index: usize,
-        grapheme: StyledGrapheme,
-    ) -> Vec<StyledGrapheme> {
+        grapheme: StyledGrapheme<'txt>,
+    ) -> Vec<StyledGrapheme<'txt>> {
         match grapheme.symbol {
-            "\n" => Self::remap_newline(grapheme),
-            "\t" => Self::remap_tab(grapheme, inline_index),
+            "\n" => self.remap_newline(grapheme),
+            "\t" => self.remap_tab(grapheme, inline_index),
             _ => vec![grapheme],
         }
     }
 
-    fn remap_tab(
+    fn remap_tab<'txt>(
+        &self,
         grapheme: StyledGrapheme,
         column_index: usize,
-    ) -> Vec<StyledGrapheme> {
+    ) -> Vec<StyledGrapheme<'txt>> {
         let tab_width = (4 - column_index % 4) as u8;
-        let style = grapheme
-            .style
-            .patch(tui::style::Style::default().fg(tui::style::Color::Yellow));
 
         let mut tab = vec![StyledGrapheme {
-            symbol: Self::TAB_SYMBOL,
-            style,
+            symbol: self.symbols.tab.symbol,
+            style: grapheme.style.patch(self.symbols.tab.style),
         }];
-
         tab.extend(vec![
-            StyledGrapheme { symbol: " ", style };
+            StyledGrapheme {
+                symbol: " ",
+                style: grapheme.style
+            };
             (tab_width - 1) as usize
         ]);
 
         tab
-
-        // vec![StyledGrapheme {
-        //     symbol: Self::TAB_SYMBOL,
-        //     style,
-        // }]
-        // .into_iter()
-        // .chain(vec![
-        //     StyledGrapheme { symbol: " ", style };
-        //     (tab_width - 1) as usize
-        // ])
-        // .collect()
     }
 
-    fn remap_newline(grapheme: StyledGrapheme) -> Vec<StyledGrapheme> {
+    fn remap_newline<'txt>(
+        &self,
+        grapheme: StyledGrapheme,
+    ) -> Vec<StyledGrapheme<'txt>> {
         vec![StyledGrapheme {
-            symbol: Self::NL_SYMBOL,
-            style: grapheme.style.patch(
-                tui::style::Style::default().fg(tui::style::Color::Yellow),
-            ),
+            symbol: self.symbols.nl.symbol,
+            style: grapheme.style.patch(self.symbols.nl.style),
         }]
     }
+}
+
+pub struct LineStylizer;
+
+impl LineProcessor for LineStylizer {
+    fn process_line<'txt>(
+        &self,
+        line: &mut dyn Iterator<Item = StyledGrapheme<'txt>>,
+        sparse_styling: HashMap<usize, tui::style::Style>,
+        width: u16,
+    ) -> Vec<Vec<StyledGrapheme<'txt>>> {
+        let yellow = |symbol| StyledGrapheme {
+            style: tui::style::Style::default().fg(tui::style::Color::Yellow),
+            symbol,
+        };
+
+        let processor = BaseLineProcessor {
+            symbols: SymbolMap {
+                tab: yellow(TAB_SYMBOL),
+                nl: yellow(NL_SYMBOL),
+            },
+        };
+
+        processor.process_line(line, sparse_styling, width)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
 }
